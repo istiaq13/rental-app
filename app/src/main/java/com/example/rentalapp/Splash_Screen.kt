167b8e7bd580da58ab6441c52bc699a60c4a62a6
Mohammad Istiaq Prodhan <istiaqprodhan02@gmail.com>
2024-08-29 20:12:25 +0600

    startup page fix & signup page minor fix

--- app/src/main/java/com/example/rentalapp/Splash_Screen.kt
+++ app/src/main/java/com/example/rentalapp/Splash_Screen.kt
@@ -1,12 +1,16 @@
 package com.example.rentalapp
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class Splash_Screen : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +19,11 @@ class Splash_Screen : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val menuButton = findViewById<Button>(R.id.start_button)
+        menuButton.setOnClickListener {
+            val intent = Intent(this, authenticationMenu::class.java)
+            startActivity(intent)
         }
     }
 }
