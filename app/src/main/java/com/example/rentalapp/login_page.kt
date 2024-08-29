6eed7e8bd95d5e7d9916f3b65cf185553de5d60d
Mohammad Istiaq Prodhan <istiaqprodhan02@gmail.com>
2024-08-30 03:00:31 +0600

    new login page

--- app/src/main/java/com/example/rentalapp/login_page.kt
+++ app/src/main/java/com/example/rentalapp/login_page.kt
@@ -1,6 +1,8 @@
 package com.example.rentalapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +18,13 @@ class login_page : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        //signup page opens through this click event
+
+        /*val makeAccButton = findViewById<textView>(R.id.make_acc)
+        makeAccButton.setOnClickListener {
+            val intent = Intent(this, sign_up_page::class.java)
+            startActivity(intent)
+        }*/
     }
 }
